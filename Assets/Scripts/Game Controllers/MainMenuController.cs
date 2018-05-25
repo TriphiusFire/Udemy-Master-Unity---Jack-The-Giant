dccedb84459998a2f4d46dc98f6723848377a8c3
TriphiusFire <jeremyscott@rocketship.com>
2018-05-24 23:26:29 -0700

    added assets, library

---
+++ Assets/Scripts/Game Controllers/MainMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void HighScoreMenu()
+    {
+        SceneManager.LoadScene("HighScoreScene");
+    }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MusicButton()
+    {
+
+    }
+}
